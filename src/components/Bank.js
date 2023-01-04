5c2bbf3429c20ac80a11843b95cd7ae9eaaac5c4
adityaKumar <adityakumarrajput01@gmail.com>
2023-01-04 21:14:00 +0530

    basic buildup

---
+++ src/components/Bank.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Bank = () => {
+    return (
+
+        <div>
+            <h2 className="my-3">Deposit / Withdraw money</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Update Balance
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Bank
