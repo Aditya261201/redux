922d3aec9ecab027b96b4adba7767cf4f3b50b3e
adityaKumar <adityakumarrajput01@gmail.com>
2023-01-05 00:19:19 +0530

    Updating state from a different component in Redux

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import {useDispatch} from 'react-redux'
+import { bindActionCreators } from 'redux';
+import {actionCreators} from '../state/index'
+
 
 const Bank = () => {
+    const dispatch = useDispatch();
+    const {withdrawMoney,depositMoney} = bindActionCreators(actionCreators,dispatch)
     return (
 
         <div>
-            <h2 className="my-3">Deposit / Withdraw money</h2>
-            <button className="btn btn-primary mx-2">-</button>
+            
+            {/* if we want to use the state without bindActionCreators ||| */}
+
+            {/* <h2 className="my-3">Deposit / Withdraw money</h2>
+            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
             Update Balance
-            <button className="btn btn-primary mx-2">+</button>
+            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button> */}
+
+
+
+            <h2 className="my-3">Deposit / Withdraw money</h2>
+            <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+            Update Balance
+            <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
+            {/* Updating state from a different component in Redux */}
         </div>
     )
 }
